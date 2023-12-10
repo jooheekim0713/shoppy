783401eb7ad9af39ad6a7ddaf1a7c688168adf22
jooheekim <jooheekim.dev@gmail.com>
2023-12-10 20:33:31 +0900

    [ STYLE ] : 버튼 공통화,버튼 CSS 수정

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import { FaShopify } from 'react-icons/fa';
 import { BsFillPencilFill } from 'react-icons/bs';
 import { login, logout, onUserStateChange } from '../api/firebase';
+import Button from './ui/Button';
+import User from './User';
 
 export default function Navbar() {
   const [user, setUser] = useState();
@@ -30,13 +32,14 @@ export default function Navbar() {
       <nav className="flex items-center gap-4 font-semibold">
         <Link to="/products">Products</Link>
         <Link to="/carts">Carts</Link>
-        {user && user.idAdmin && (
+        {user && user.isAdmin && (
           <Link to="/products/new" className="text-2xl">
             <BsFillPencilFill />
           </Link>
         )}
-        {!user && <button onClick={handleLogin}>Login</button>}
-        {user && <button onClick={handleLogout}>Logout</button>}
+        {user && <User user={user} />}
+        {!user && <Button text={'Login'} onClick={handleLogin} />}
+        {user && <Button text={'Logout'} onClick={handleLogout} />}
       </nav>
     </header>
   );
