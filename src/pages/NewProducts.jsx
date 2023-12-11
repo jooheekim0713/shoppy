080e51d7b2a2cbfe5a4871c1d2b2a3af6439746f
jooheekim <jooheekim.dev@gmail.com>
2023-12-11 17:11:26 +0900

    [ STYLE ] : 제품등록화면 CSS수정

--- src/pages/NewProducts.jsx
+++ src/pages/NewProducts.jsx
@@ -33,63 +33,71 @@ export default function NewProducts() {
       .finally(() => setIsUploading(false));
   };
   return (
-    <section>
-      <h2>새로운 제품 등록</h2>
-      {success && <p>{success}</p>}
-      {file && <img src={URL.createObjectURL(file)} alt="local file" />}
-      <form onSubmit={handleSubmit}>
-        <input
-          type="file"
-          accept="image/*"
-          name="file"
-          required
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="title"
-          value={product.title ?? ''}
-          placeholder="제품명"
-          required
-          onChange={handleChange}
-        />
-        <input
-          type="number"
-          name="price"
-          value={product.price ?? ''}
-          placeholder="가격"
-          required
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="category"
-          value={product.category ?? ''}
-          placeholder="카테고리"
-          required
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="description"
-          value={product.description ?? ''}
-          placeholder="제품 설명"
-          required
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="options"
-          value={product.options ?? ''}
-          placeholder="옵션들(콤마(,)로 구분)"
-          required
-          onChange={handleChange}
-        />
-        <Button
-          text={isUploading ? '업로드중 ...' : '제품 등록하기'}
-          disabled={isUploading}
-        />
-      </form>
+    <section className="w-full text-center">
+      <h2 className="text-2xl font-bold my-4">새로운 제품 등록</h2>
+      {success && <p className="my-2">{success}</p>}
+      <div className="flex flex-row justify-center items-center">
+        {file && (
+          <img
+            className="w-96 mb-2"
+            src={URL.createObjectURL(file)}
+            alt="local file"
+          />
+        )}
+        <form className="flex flex-col px-12 block" onSubmit={handleSubmit}>
+          <input
+            type="file"
+            accept="image/*"
+            name="file"
+            required
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="title"
+            value={product.title ?? ''}
+            placeholder="제품명"
+            required
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            name="price"
+            value={product.price ?? ''}
+            placeholder="가격"
+            required
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="category"
+            value={product.category ?? ''}
+            placeholder="카테고리"
+            required
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="description"
+            value={product.description ?? ''}
+            placeholder="제품 설명"
+            required
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            name="options"
+            value={product.options ?? ''}
+            placeholder="옵션들(콤마(,)로 구분)"
+            required
+            onChange={handleChange}
+          />
+          <Button
+            text={isUploading ? '업로드중 ...' : '제품 등록하기'}
+            disabled={isUploading}
+          />
+        </form>
+      </div>
     </section>
   );
 }
