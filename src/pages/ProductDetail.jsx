7c6dfacf30b56f88a793896561897e35f5c72bd3
jooheekim <jooheekim.dev@gmail.com>
2023-12-11 21:34:57 +0900

    [ FEAT ] : 제품 상세화면 구현
    ProductCard 컴포넌트 클릭시 ProductDetail 컴포넌트로 이동, ProductDetail에서 제품 데이터 보여줌

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,5 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import Button from '../components/ui/Button';
 
 export default function ProductDetail() {
-  return <div>product detail</div>;
+  const {
+    state: {
+      product: { id, image, title, description, category, price, options },
+    },
+  } = useLocation();
+  const [selected, setSelected] = useState(options && options[0]);
+  const handleSelect = (e) => {
+    setSelected(e.target.value);
+  };
+  const handleClick = () => {
+    //장바구니 추가하기 로직 구현
+  };
+  return (
+    <section>
+      <p>{category}</p>
+      <img src={image} alt={title} />
+      <div>
+        <h2>{title}</h2>
+        <p>{price}</p>
+        <p>{description}</p>
+        <p>옵션 : </p>
+        <select onChange={handleSelect} value={selected}>
+          {options &&
+            options.map((option, index) => (
+              <option key={index}>{option}</option>
+            ))}
+        </select>
+        <Button text="장바구니에 추가" onClick={handleClick} />
+      </div>
+    </section>
+  );
 }
