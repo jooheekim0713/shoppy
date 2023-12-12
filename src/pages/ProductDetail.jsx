516b4a8488e34c83c92869969cf53ce2da780ee4
jooheekim <jooheekim.dev@gmail.com>
2023-12-12 16:18:00 +0900

    [ FEAT ] : 제품 구매시 장바구니 등록 알림 구현

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -10,13 +10,21 @@ export default function ProductDetail() {
       product: { id, image, title, description, category, price, options },
     },
   } = useLocation();
+  const [success, setSuccess] = useState();
   const [selected, setSelected] = useState(options && options[0]);
   const handleSelect = (e) => {
     setSelected(e.target.value);
   };
   const handleClick = () => {
     const product = { id, image, title, price, option: selected, quantity: 1 };
-    addOrUpdateItem.mutate(product);
+    addOrUpdateItem.mutate(product, {
+      onSuccess: () => {
+        setSuccess('장바구니에 추가되었습니다.');
+        setTimeout(() => {
+          setSuccess(null);
+        }, 3000);
+      },
+    });
   };
   return (
     <section>
@@ -45,6 +53,7 @@ export default function ProductDetail() {
                 ))}
             </select>
           </div>
+          {success && <p className="mb-2">{success}</p>}
           <Button text="장바구니에 추가" onClick={handleClick} />
         </div>
       </section>
