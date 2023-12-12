c103ea4ffad9da12c86c6804340f82e43e28d168
jooheekim <jooheekim.dev@gmail.com>
2023-12-12 15:26:25 +0900

    [ REFACTOR ] : 커스텀 훅을 사용해 UI로직과 비즈니스 로직 분리
    Products, NewProducts의 데이터 fetching로직을 useProducts훅으로 관리해 비즈니스 로직과 UI로직 분리

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import { useQuery } from '@tanstack/react-query';
-import { getProducts } from '../api/firebase';
 import ProductCard from './ProductCard';
+import useProducts from '../hooks/useProducts';
 
 export default function Products() {
   const {
-    isLoading,
-    error,
-    data: products,
-  } = useQuery({
-    queryKey: ['products'],
-    queryFn: getProducts,
-    staleTime: 1000 * 6,
-  });
+    productsQuery: { isLoading, error, data: products },
+  } = useProducts();
   return (
     <>
       {isLoading && <p>Loading...</p>}
